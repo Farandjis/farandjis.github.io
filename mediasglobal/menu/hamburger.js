86038706bc688623ef355f6d1cf1528d5552b8c0
Matthieu <matthieu.farandjis@orange.fr>
2024-02-19 12:43:42 +0100

    menu fonctionnel pc et mobile, manque disposition et amélioration à faire

---
+++ mediasglobal/menu/hamburger.js
@@ -0,0 +1,26 @@
+function hamburger() {
+    var nav = document.querySelector("nav");
+    var btnToggleNav = document.querySelector(".hamburger-menu");
+
+    nav.classList.toggle("active");
+    btnToggleNav.classList.toggle("active");
+
+    // --- mon code
+
+    // Accéder au document de la page parente
+    var documentParent = parent.document;
+    var nbDeBoutonDuMenuHaut = document.getElementsByClassName("centrer").item(0).childElementCount;
+
+    // Faire quelque chose avec le document de la page parente
+    var iframe = documentParent.getElementsByClassName("menu_haut").item(0);
+    var hautCSS = documentParent.getElementsByClassName("HautCSS").item(0);
+    if (iframe.getAttribute("style") == null) {
+        iframe.setAttribute("style", "height: " + nbDeBoutonDuMenuHaut*53 + "px;");
+        hautCSS.setAttribute("style", "top: " + nbDeBoutonDuMenuHaut*26 + "px;");
+    }
+    else {
+        iframe.removeAttribute("style");
+        hautCSS.removeAttribute("style");
+    }
+
+}
